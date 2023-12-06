50f28b26aca45171287e009d3cf48705000a10ac
Anna-Rayevskaya <89201707419sh@gmail.com>
2023-12-06 12:41:02 +0300

    готово-2-3-14

--- src/services/rating.js
+++ src/services/rating.js
@@ -1,24 +1,4 @@
 export default class Rating {
-  // async addRating(idSession, idFilm) {
-  //   const url = `https://api.themoviedb.org/3/movie/${idFilm}/rating?guest_session_id=${idSession}`
-
-  //   const res = await fetch(url, {
-  //     method: 'POST',
-  //     headers: {
-  //       Authorization:
-  //         'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMmY1MDFhOTI0NDU0ODAxZDliYTE4YWRlY2VjZDY1MSIsInN1YiI6IjY1NTRiYTI4MDgxNmM3MDBlMDE5MDJiMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yq0N-JkxyoymriAVOX_XOdIc4UKTTVx6mGCaIUwq60g',
-  //       Accept: 'application/json',
-  //       'Content-Type': 'application/json;charset=utf-8',
-  //     },
-  //     body: JSON.stringify({ value: 4 }),
-  //   })
-  //   if (!res.ok) {
-  //     throw new Error(`Could not fetch, received ${res.status}`)
-  //   }
-
-  //   return await res.json()
-  // }
-
   async getRatedMovies(idSession) {
     const url = `https://api.themoviedb.org/3/guest_session/${idSession}/rated/movies?language=en-US&page=1&sort_by=created_at.asc`
 
@@ -37,17 +17,8 @@ export default class Rating {
     return await res.json()
   }
 
-  // async addRatingFilm(idSession, idFilm) {
-  //   const res = await this.addRating(idSession, idFilm)
-  //   // eslint-disable-next-line no-console
-  //   console.log(res)
-  //   return res
-  // }
-
   async geRatedFilms(idSession) {
     const res = await this.getRatedMovies(idSession)
-    // eslint-disable-next-line no-console
-    console.log(res)
     return res
   }
 }
